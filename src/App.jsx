d4969b3129cb965fd8c859234569b74fe47e6857
Kaianvs <kaianvs12@gmail.com>
2025-05-19 10:44:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Tasks from "./components/Tasks"
+
+function App(){
+  // State (ESTADO) de um componente
+  // Hook
+  
+  return(
+    <div>
+      <h1>Gerenciador de tarefas</h1>
+      <Tasks/>
+    </div>
+  )
+}
+
+export default App
